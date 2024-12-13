d20b22138256d48d178031154ae0cfee7bccf71e
Daniel Kim <dtk.danielkim@gmail.com>
2024-12-12 18:17:17 -0800

    Delete checklist function because there accidentally was two

--- bird_watching_app/controllers.py
+++ bird_watching_app/controllers.py
@@ -68,11 +68,6 @@ def checklist():
 def location():
     return dict()
 
-@action('my_checklists', method=['GET'])
-@action.uses('checklist.html', auth.user)
-def checklist():
-    return dict()
-
 @action('heatmap_data', method=['GET'])
 @action.uses(db, auth.user)
 def heatmap_data():
